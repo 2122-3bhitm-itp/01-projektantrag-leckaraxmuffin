3de2d378414acdbe703f010efa5eff8426e29913
leckaraxmuffin <l.maslowski@gmx.at>
2021-10-20 19:35:25 +0200

    - rebuilding gh-pages Wed Oct 20 07:35:25 PM CEST 2021

--- run-inside-docker.sh
+++ run-inside-docker.sh
@@ -3,10 +3,11 @@ BUILD_DIR="gh-pages"
 echo "BUILD_DIR->${BUILD_DIR}"
 OLD=${PWD}
 echo "OLD->${OLD}"
+echo "PWD->${PWD}"
 docker run --rm \
            -v ${PWD}:/documents \
            --env-file ./.env \
-           asciidoctor/docker-asciidoctor:1.2.0  /bin/bash -c "/documents/publish-gh-pages-inside-docker.sh"
+           asciidoctor/docker-asciidoctor:1.2.0  /bin/bash -c "${PWD}/publish-gh-pages-inside-docker.sh"
            #asciidoctor/docker-asciidoctor:1.2.0  /bin/bash -c "/documents/build-html-docker-inside-docker.sh"
 cd $PWD/$BUILD_DIR
 CURRENT_FOLDER=${PWD}
